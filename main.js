89f6554c3cde587c2cda01d879fc159f3ed06368
lyli127 <lylibete@gmail.com>
2023-03-26 14:22:46 +1100

    more user input form validation

---
+++ main.js
@@ -0,0 +1,10 @@
+/* 
+//Get all elements from the DOM I want to manipulate each letter from each row
+
+//create any elements I'll need to show in the DOM
+
+//Sanitise user input
+- check it it's exactly 5 letters
+- make them uppercase 
+
+*/
