020d8eed76358ce919e66ee8c5e1f4bbc3c3ac4d
lyli127 <lylibete@gmail.com>
2023-03-27 14:13:36 +1100

    add event listeners

--- main.js
+++ main.js
@@ -109,3 +109,7 @@ function updateScore() {
   score.innerText = newScore;
 }
 updateScore();
+
+//Add Event Listeners
+guessWordBtn.addEventListener(onclick, onGuessWordBtnClick);
+resetGameBtn.addEventListener(onclick, resetBoard);
