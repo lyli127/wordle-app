b0800240be3cc22ef198648e8f531114c04d16d2
lyli127 <lylibete@gmail.com>
2023-03-28 20:11:58 +1100

    add new confetti style

--- main.js
+++ main.js
@@ -41,7 +41,43 @@ function checkGuessedWord(guessedWord) {
 
   if (guessedWord === randomWord) {
     document.getElementById("guessWordBtn").disabled = true;
-    confetti();
+    //Confetti sourced from https://www.kirilv.com/canvas-confetti/
+    // confetti();
+    var count = 200;
+    var defaults = {
+      origin: { y: 0.7 },
+    };
+
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio),
+        })
+      );
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
     newScore = newScore + 1;
   }
 
