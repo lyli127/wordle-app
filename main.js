6206ee7b39feac93a884b7a0f1e5c44807203daa
lyli127 <lylibete@gmail.com>
2023-03-28 20:09:37 +1100

    remove comments and console log

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-/* 
-//Get all elements from the DOM I want to manipulate each letter from each row
-- get words array from GA ✓
-- get all letters ✓
-- get the user input value ✓
-- store attempts number which equals to valid button clicks with valid inputs ✓
-- store score value = 0 ✓
-
-//create any elements I'll need to show in the DOM
-
-//grab random word from array with math.random math.floor ✓
-
-// Create function to sanitise user input
-- check it it's exactly 5 letters ✓
-- make the letters uppercase ✓
-- check if word exists in array, if not return invalid word message ✓
-
-//Create function to check attempt number
-- On attempt number 5, the 'guess word' button must be disabled
-- If onButton click attempt number = 5; then alert user has lost
-- If onButton click attempt number < 5; then make sure to print the word in the next row
-
-//Check if guessedWord === randomWord 
-- if guessedWord === randomWord, then user wins and score ++ and print the word in green on the board
-- if guessedWord character is included in randomWord but have != indexes then print letter in yellow on the board
-- if guessedWord character index === randomWord character index then print letter in green on the board
-
-//Create function (and button) to reset game/board but not the score (clearboard function)
-- get every letter element and set text.content to empty string and clean up any added classes classes.remove("guessedWord"); ✓
-- remove 'disabled' button attribute
-
-//Set event listeners
-
-*/
 //Get DOM Elms
 
 const letters = document.querySelectorAll(".letter");
@@ -63,7 +29,6 @@ function sanitise() {
   ) {
     guessedWord = guessedWord.toUpperCase();
     attemptNum = attemptNum + 1;
-    console.log(guessedWord);
     return guessedWord;
   } else {
     alert(`${guessedWord} is not a valid word. Please try again.`);
@@ -73,25 +38,19 @@ function sanitise() {
 //Testing characters - Game logic
 function checkGuessedWord(guessedWord) {
   userInput.value = "";
-  // guessedWord = "ABASE";
-  // randomWord = "ABASE";
-  console.log(guessedWord);
+
   if (guessedWord === randomWord) {
-    // debugger;
     document.getElementById("guessWordBtn").disabled = true;
     confetti();
     newScore = newScore + 1;
   }
 
   for (let index = 0; index < 5; index++) {
-    // debugger;
     //character is right and in the right position
-    // console.log(guessedWord[index]);
     if (guessedWord[index] === randomWord[index]) {
       const squareToUpdate = document.querySelector(
         `.row${attemptNum}.letter${index + 1}`
       );
-      console.log(squareToUpdate);
       squareToUpdate.classList.add("rightLetter");
       squareToUpdate.innerText = guessedWord[index];
       //character is right and in the wrong position
@@ -141,7 +100,6 @@ function updateScore() {
 
 //Attempt Number
 function attemptTracker(guessedWord) {
-  // debugger;
   if (attemptNum === 5 && randomWord !== guessedWord) {
     document.getElementById("guessWordBtn").disabled = true;
     alert("You lost");
@@ -153,13 +111,10 @@ function attemptTracker(guessedWord) {
 //Main function for the game
 function onGuessWordBtnClick(event) {
   event.preventDefault();
-  console.log(randomWord);
   let sanitisedGuess = sanitise();
-  // console.log(guessedWord);
   checkGuessedWord(sanitisedGuess);
   updateScore();
   attemptTracker(sanitisedGuess);
-  console.log(newScore);
 }
 
 //Add Event Listeners
@@ -170,4 +125,3 @@ startNewGameBtn.addEventListener("click", initialiseBoard);
 //Calling funtions
 updateScore();
 initialiseBoard();
-console.log(randomWord);
